06902805beb8c1000e6bcfc87b711b4a86a24053
Tim Urista <timothy.urista@gmail.com>
2021-02-23 09:51:10 -0800

    initial setup for rl trader

---
+++ rl_trader/start.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+# DATA=$(cat .data/)
+# if [-z "$DATA"]; then
+#     wget https://raw.githubusercontent.com/lazyprogrammer/machine_learning_examples/master/tf2.0/aapl_msi_sbux.csv
+# fi
+
+source .venv/bin/activate
+python rl_trader.py
